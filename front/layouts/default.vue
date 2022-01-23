d64d6fdc839c450aac570597c3a68475484e1197
esasova <sassova.elena@gmail.com>
2022-01-23 10:57:11 +0100

    feat/add nuxt front

---
+++ front/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <nuxt/>
+</div>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ front/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="font-bold">
+    My future homepage
+  </div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    this.$axios.$get('http://127.0.0.1:8000/api/coffeeshops.json')
+    .then((response) => {
+      console.log(response)
+    })
+  }
+}
+</script>
