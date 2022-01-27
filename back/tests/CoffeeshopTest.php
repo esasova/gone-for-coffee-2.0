ddd17ba6c28c3340d212aa673e9adf3f7c6f82de
esasova <sassova.elena@gmail.com>
2022-01-27 12:31:19 +0100

    feat:add coffeeshop entity test

--- back/tests/CoffeeshopTest.php
+++ back/tests/CoffeeshopTest.php
@@ -3,16 +3,26 @@
 namespace App\Tests;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\Coffeeshop;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
 
 class CoffeeshopTest extends ApiTestCase
 {
-    public function testSomething(): void
+    use RefreshDatabaseTrait;
+
+    public function testGetCollection(): void
     {
         $response = static::createClient()->request('GET', '/api/coffeeshops');
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
 
         $this->assertResponseIsSuccessful();
-        $this->assertJsonContains(['@id' => '/api/coffeeshops']);
-
+        $this->assertJsonContains([
+                        '@context' => '/api/contexts/Coffeeshop',
+                        '@id' => '/api/coffeeshops',
+                        '@type' => 'hydra:Collection',
+                        'hydra:totalItems' => 10,
+                    ]);
         $this->assertCount(10, $response->toArray()['hydra:member']);
+        $this->assertMatchesResourceCollectionJsonSchema(Coffeeshop::class);
     }
 }
